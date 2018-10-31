203ceb3ec935f7d4d75af10b2a803eb89a683a15
Davi <ivadcgs1999@gmail.com>
2018-10-31 08:58:12 -0300

    Telas
    Refatorei as Telas

--- GestãoHoteleira/src/pessoa/Hospede.java
+++ GestãoHoteleira/src/pessoa/Hospede.java
@@ -1,5 +1,16 @@
 package pessoa;
 
+
 public class Hospede extends Papel {
+    private String numeroCartao;
+    
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
     
 }
