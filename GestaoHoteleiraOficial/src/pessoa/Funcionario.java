93d26ecac08fb3bb1551d5b0b62412b16c338e09
Lunathos <ivadcgs1999@gmail.com>
2018-11-07 17:22:39 -0200

    Adicionando algumas classes
    123

---
+++ GestaoHoteleiraOficial/src/pessoa/Funcionario.java
@@ -0,0 +1,34 @@
+package pessoa;
+
+public abstract class Funcionario extends Papel{
+    
+    private String cargo;
+    private double salario;
+    private boolean atividade = false;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public boolean isAtividade() {
+        return atividade;
+    }
+
+    public void setAtividade(boolean atividade) {
+        this.atividade = atividade;
+    }
+    
+    
+}
